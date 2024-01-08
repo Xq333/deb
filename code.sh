f41652bef2b6b3fd78552583cea4481718f995ac
Xq333 <xq1.3x3@gmail.com>
2024-01-08 17:36:19 +0100

    update telescope (grep from astronvim)

--- code.sh
+++ code.sh
@@ -57,3 +57,7 @@ sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt install ripgrep
+
+sudo apt install fd-find
