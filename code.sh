609784a97e053064151bb0835c69e4237d077572
Xq333 <xq1.3x3@gmail.com>
2024-01-08 10:45:24 +0100

    update main

--- code.sh
+++ code.sh
@@ -53,3 +53,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ${USER}
 
 sudo systemctl start docker
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
